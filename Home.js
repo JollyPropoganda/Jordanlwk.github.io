65eeeab6e82e255755a3c74a70414b000e03f02d
JollyPropoganda <74634228+JollyPropoganda@users.noreply.github.com>
2024-01-29 21:25:44 +0800

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,19 @@
+function redirect()
+{
+    window.location.href = "H̵̨̢͖̲̳̗̪̠̰͚͓͙̜͇͒̿̊̒͛͗̌͂̆̓̑͝ͅa̴͉̼̺̖̜͍͙̞̭̩̗͂͌͗̚c̸̡̛͉̹͚͉̩̙̘̬̝͉͕͔̖͌͛̒͆̃͆͛̍ͅk̴̖̫͖̠̺̯͚͈̳̊͝ͅỉ̵͉̰̿́n̵͈̬̲̯̺͉̰͙͖̙̯̖͑̄̓̕ͅg̶̡̢̢̛͍̹͈͙̤̭͇̓͆̃̑́̄̉̈́̏̔͜͝͝͝.html";
+}
+
+function sendtoabout()
+{
+    window.location.href = "Hacking_Portfolio.html";
+}
+
+function sendtoProjects()
+{
+    window.location.href = "Project.html";
+}
+
+function play_audio() 
+{
+    var x = document.getElementById("myAudio").autoplay;
+}
---
+++ bootstrap.js
@@ -0,0 +1,9 @@
+function Close() {
+    document.getElementById('dropdown_list').style.visibility = 'hidden';
+    document.getElementById('body').style.overflow = 'scroll'
+}
+
+function show() {
+    document.getElementById('dropdown_list').style.visibility = 'visible';
+    document.getElementById('body').style.overflow = 'hidden'
+}
