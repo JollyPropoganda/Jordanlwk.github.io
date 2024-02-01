df0822b4ffd09c7e283c6c089395c478cfc8799a
JollyPropoganda <74634228+JollyPropoganda@users.noreply.github.com>
2024-02-01 17:18:31 +0800

    update proj 2

---
+++ Projects/HA_Proj.js
@@ -0,0 +1,12 @@
+function openup() {
+  var dropdown = document.getElementById('Sprint1_container');
+  dropdown.style.visibility = "visible";
+  dropdown.style.display = "block";
+  dropdown.style.height = "fit-content";
+  dropdown.style.width = "fit-content";
+  dropdown.style.transition = "max-height 0.2 ease-in"
+}
+
+function closeup() {
+  
+}
